b6774ab59277aafdf2367af26f5cb09680c6ef71
rongill <32289563+rongill@users.noreply.github.com>
2018-10-10 09:27:50 +0300

    working version 10.10.18 1. Replaced user.getDisplayName().toString() to user.getEmail() on MainActivity.java
    2. changed onclike method on signup/login

--- app/src/main/java/com/rongill/rsg/sinprojecttest/MainActivity.java
+++ app/src/main/java/com/rongill/rsg/sinprojecttest/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, LoginActivity.class));
             finish();
         }else{
-            mUsername.setText(user.getDisplayName());
+            mUsername.setText(user.getEmail());
         }
     }
 
